1da288e0b4256e6d878c97a730ef45e2c82dc859
Marcosino4 <morenolpz.marcos@gmail.com>
2025-12-07 21:16:22 +0100

    Fix for github pages

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,12 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
+import { ApplicationConfig } from '@angular/core';
+import { provideRouter, withHashLocation } from '@angular/router';
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    // Añadimos withHashLocation() aquí dentro
+    provideRouter(routes, withHashLocation()), 
+    provideAnimationsAsync()
   ]
 };
